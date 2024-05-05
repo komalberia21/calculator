d3baa4786cb26bae275a3822c80bda1f2c33def2
komalberia21 <komal.beria21@gmail.com>
2024-05-05 21:29:32 +0530

    changes

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,12 +1,13 @@
 import { useContext } from "react"
 import {CalcContext} from "../context/CalcContext"
-import { Textfit } from "react-textfit";
+//import { Textfit } from "react-textfit";
 
 const Screen = () => {
   const{calc}=useContext(CalcContext);
 
   return (
-    <Textfit className="screen" max={70} mode="single">{calc.curr?calc.curr:calc.res}</Textfit>
+    // <Textfit className="screen" max={70} mode="single">{calc.curr?calc.curr:calc.res}</Textfit>
+    <div className="screen">{calc.curr?calc.curr:calc.res}</div>
   )
 }
 
